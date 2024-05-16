0943f4605358d31ad928e6f13de9f811136bdc80
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-16 00:23:40 -0700

    Add each pump to it

--- Utils/VehicleUtility.cs
+++ Utils/VehicleUtility.cs
@@ -240,6 +240,7 @@ namespace SimpleCTRL.Utils
 										float resultArg = N.GetGroundZFor3DCoord(pump.Position.X, pump.Position.Y, 1000f, false);
 										pump.Position.Z = resultArg;
 										Rage.Object obj = new Rage.Object(model.Hash, pump.Position);
+										Globals.DepartmentPumpObjects.Add(obj);
 										NativeFunction.CallByHash<int>(0x8524A8B0171D5E07, obj, 0.0f, 0.0f, Common.API.Math.DirectionToRotation(Common.API.Math.HeadingToDirection(pump.Rotation), 0f).Z, 1);
 										if (EntityExtensions.Exists(obj)) 
 										{
