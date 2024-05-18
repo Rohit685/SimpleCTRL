079fda73cf1b6f3264fce18361bba7aeb7fd2bbd
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-18 03:59:28 -0700

    Added two new helpers

--- Utils/Helpers.cs
+++ Utils/Helpers.cs
@@ -1,7 +1,9 @@
-﻿using Common;
-using Rage;
+﻿using Rage;
+using RAGENativeUI;
+using SimpleCTRL.Handlers;
 using System;
-using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
 
 namespace SimpleCTRL.Utils
 {
@@ -24,6 +26,67 @@ namespace SimpleCTRL.Utils
         static UIHelper()
         {
             GameFiber.StartNew(Process, "SimpleCTRL - UI Helper");
+        }
+    }
+
+    internal static class ConversionAndFormattingHelper
+    {
+        #region String to Integer Conversion
+        public static int ToInt32(this string text, [CallerMemberName] string callingMethod = null)
+        {
+            int i = 0;
+            try
+            {
+                i = Convert.ToInt32(text);
+            }
+            catch (Exception e)
+            {
+                Game.LogTrivial(e.Message);
+            }
+            return i;
+        }
+        #endregion
+
+        #region Key Binding Formatting
+        public static string FormatKeyBinding(ControllerButtons key) => $"{key.GetInstructionalId()}";
+
+        public static string FormatKeyBinding(Keys key) => $"{key.GetInstructionalId()}";
+        #endregion
+    }
+
+    internal static class VehicleHelper
+    {
+        public static RepairShop GetNearestRepairShop()
+        {
+            foreach (RepairShop repairShop in ConfigHandler.RepairShops)
+            {
+                if (Game.LocalPlayer.Character.DistanceTo(repairShop.Position) < repairShop.UseRange)
+                {
+                    return repairShop;
+                }
+            }
+            return null;
+        }
+
+        public static int GetBrakePressure(int pressure)
+        {
+            if (pressure > 230)
+            {
+                return 10;
+            }
+            else if (pressure > 205)
+            {
+                return 8;
+            }
+            else if (pressure > 180)
+            {
+                return 6;
+            }
+            else if (pressure > 155)
+            {
+                return 4;
+            }
+            return 2;
         }
     }
 }
