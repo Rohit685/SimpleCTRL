3d9c05aa66148e400c96c3c6e0633e89aa23ded8
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-27 18:50:05 -0700

    New UI Method for Manual refueling

--- UI/CustomUI.cs
+++ UI/CustomUI.cs
@@ -201,6 +201,17 @@ namespace SimpleCTRL.UI
 			buttons.CallFunction("DRAW_INSTRUCTIONAL_BUTTONS", -1);
 		}
 
+		public static void InstructManualRefuelOrSiphon()
+        {
+			buttons.Load("instructional_buttons");
+			buttons.CallFunction("CLEAR_ALL");
+			buttons.CallFunction("TOGGLE_MOUSE_BUTTONS", 0);
+			buttons.CallFunction("CREATE_CONTAINER");
+			buttons.CallFunction("SET_DATA_SLOT", 0, N.GetControlInstructionalButtonsString(2, 24, false), "Manual refueling");
+			buttons.CallFunction("SET_DATA_SLOT", 1, N.GetControlInstructionalButtonsString(2, 25, false), "Siphon");
+			buttons.CallFunction("DRAW_INSTRUCTIONAL_BUTTONS", -1);
+		}
+
 		// possibly change in future but fine for now should use instructfull or empty
 		public static void HideRefuel()
         {
