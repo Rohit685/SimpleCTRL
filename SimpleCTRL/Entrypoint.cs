80823a0c8a1f2ee22ea6aea595cbdc00d815bac0
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2025-05-08 01:11:14 -0400

    a

--- SimpleCTRL/Entrypoint.cs
+++ SimpleCTRL/Entrypoint.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleCTRL
+﻿namespace SimpleCTRL
 {
     public class Entrypoint
     {
+        public static void Main()
+        {
+            InitializePlugin();
+        }
+
+        private static void InitializePlugin()
+        {
+            // This section intentionally left blank.
+        }
+
+        public static void OnUnload(bool isTerminating)
+        {
+            // This section intentionally left blank.
+        }
     }
 }
