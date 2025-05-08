ec26fea6e52ec144a59676fc39f6578d9609b9ee
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2025-05-08 01:32:34 -0400

    Update Entrypoint.cs

--- SimpleCTRL/Entrypoint.cs
+++ SimpleCTRL/Entrypoint.cs
@@ -1,20 +1,70 @@
-﻿namespace SimpleCTRL
+﻿using System.Collections.Generic;
+
+[assembly: Rage.Attributes.Plugin("SimpleCTRL", Author = "Venoxity Development", PrefersSingleInstance = true, ShouldTickInPauseMenu = true, SupportUrl = "https://discord.gg/jCEdAF8AQz")]
+namespace SimpleCTRL
 {
-    public class Entrypoint
+    public class Entrypoint : CommonPlugin
     {
+        #region Fields
+        private static readonly Dictionary<string, DecoratorType> decorators = new Dictionary<string, DecoratorType>()
+        {
+            { "brakeHeat", DecoratorType.Int }
+        };
+        #endregion
+
+        #region Plugin Lifecycle        
         public static void Main()
         {
+            Logging.Info("SimpleCTRL plugin lifecycle started.", "EntryPoint");
+
+            DependencyManager.AddDependency("Venoxity.Common.dll", "1.0.8");
+            if (!DependencyManager.CheckDependencies()) return;
+
             InitializePlugin();
         }
+        #endregion
 
+        #region Plugin Initialization
         private static void InitializePlugin()
         {
-            // This section intentionally left blank.
-        }
+            Logging.Info("Initializing SimpleCTRL...", "EntryPoint");
 
+            try
+            {
+                Settings.Initialize();
+                Decorators.Initialize();
+                Decorators.Register(decorators);
+
+                Logging.Info("SimpleCTRL successfully initialized.", "EntryPoint");
+            }
+            catch (Exception ex)
+            {
+                Logging.Error($"Initialization failed: {ex.Message}", "EntryPoint");
+            }
+        }
+        #endregion
+
+        #region Unload
         public static void OnUnload(bool isTerminating)
         {
-            // This section intentionally left blank.
+            Logging.Info($"Plugin unloading initiated.", "EntryPoint");
+
+            try
+            {
+                CleanUp();
+
+                Logging.Info("Plugin successfully unloaded.", "EntryPoint");
+            }
+            catch (Exception ex)
+            {
+                Logging.Error($"Error during unload: {ex.Message}", "EntryPoint");
+            }
         }
+
+        private static void CleanUp()
+        {
+            Logging.Info("Cleaning up resources...", "Entrypoint");
+        }
+        #endregion
     }
 }
