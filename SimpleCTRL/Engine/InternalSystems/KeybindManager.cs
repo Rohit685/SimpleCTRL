561711fde33729ada5706ef258c46ab2383667d9
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2025-05-08 20:02:44 -0400

    Base KeybIndManager file

---
+++ SimpleCTRL/Engine/InternalSystems/KeybindManager.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleCTRL.Engine.InternalSystems
+{
+    internal class KeybindManager : CommonPlugin
+    {
+        public static void Start()
+        {
+            Logging.Info("Initializing input and keybind monitoring threads...", "KeybindManager");
+            GameFiber.StartNew(Run, "SimpleCTRL - Keybind Monitor");
+            Logging.Info("User input processing thread successfully initialized.", "KeybindManager");
+            Logging.Info("Keybind monitoring thread started.", "KeybindManager");
+        }
+
+        public static void Run()
+        {
+            while (true)
+            {
+                MainLoop();
+                GameFiber.Yield();
+            }
+        }
+
+        private static void MainLoop()
+        {
+            // This section intentionally left blank.
+        }
+    }
+}
