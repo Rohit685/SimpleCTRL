13c1e92969b515aeb1169e8bdafe5f2ec5eef67c
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2025-05-10 21:37:51 -0400

    add new global using

--- SimpleCTRL/GlobalUsings.cs
+++ SimpleCTRL/GlobalUsings.cs
@@ -7,4 +7,5 @@ global using Common.API;
 global using Common.Native;
 global using Rage;
 global using Rage.Native;
+global using SimpleCTRL.Engine.FrontendSystems.UI;
 global using SimpleCTRL.Engine.InternalSystems;
